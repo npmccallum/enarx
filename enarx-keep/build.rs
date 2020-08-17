e2102c39c08e98845a7333e63247960250d3ccb6
Harald Hoyer <harald@redhat.com>
2020-08-17 19:27:28 +0200

    Revamp the build script
    - create the output directory
    - filter the environment passed to `cargo` to have a clean build
    - rename the shim subdirs according to the crate name
    - use `cargo build` instead of `cargo install`
    - output to `/dev/tty` if useable
    - fail, if the status != 0
    - move the `cargo build` resulting binary to $OUT_DIR/bin

--- enarx-keep/build.rs
+++ enarx-keep/build.rs
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
-use std::path::Path;
-use std::process::Command;
+use std::collections::HashMap;
+use std::fs::OpenOptions;
+use std::path::{Path, PathBuf};
+use std::process::{Command, Stdio};
 
 fn rerun_src(path: impl AsRef<Path>) -> std::io::Result<()> {
     for entry in std::fs::read_dir(path)? {
@@ -32,34 +34,94 @@ fn main() {
     println!("cargo:rerun-if-env-changed=OUT_DIR");
     println!("cargo:rerun-if-env-changed=PROFILE");
 
-    let out_dir = std::env::var("OUT_DIR").unwrap();
+    let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    let out_dir_bin = out_dir.join("bin");
+
+    match std::fs::create_dir(&out_dir_bin) {
+        Err(e) if e.kind() == std::io::ErrorKind::AlreadyExists => {}
+        Err(e) => {
+            eprintln!("Can't create {:#?} : {:#?}", out_dir_bin, e);
+            std::process::exit(1);
+        }
+        Ok(_) => {}
+    }
 
     let profile: &[&str] = match std::env::var("PROFILE").unwrap().as_str() {
-        "debug" => &["--debug"],
+        "release" => &["--release"],
         _ => &[],
     };
 
+    let target_name = "x86_64-unknown-linux-musl";
+
+    let filtered_env: HashMap<String, String> = std::env::vars()
+        .filter(|&(ref k, _)| {
+            k == "TERM" || k == "TZ" || k == "LANG" || k == "PATH" || k == "RUSTUP_HOME"
+        })
+        .collect();
+
     for entry in std::fs::read_dir("shims").unwrap() {
-        let path: String = entry
+        let shim_path = entry.unwrap().path();
+        let shim_name = shim_path.clone();
+        let shim_name = shim_name
+            .file_name()
             .unwrap()
-            .path()
-            .into_os_string()
+            .to_os_string()
             .into_string()
             .unwrap();
+
+        let shim_out_dir = out_dir.join(&shim_path);
+
+        let target_dir = shim_out_dir.clone().into_os_string().into_string().unwrap();
+
+        let path: String = shim_path.into_os_string().into_string().unwrap();
 
         println!("cargo:rerun-if-changed={}/Cargo.toml", path);
         println!("cargo:rerun-if-changed={}/Cargo.lock", path);
         println!("cargo:rerun-if-changed={}/link.json", path);
         rerun_src(&path).unwrap();
 
-        Command::new("cargo")
-            .arg("install")
+        let stdout: Stdio = OpenOptions::new()
+            .write(true)
+            .open("/dev/tty")
+            .map(Stdio::from)
+            .unwrap_or_else(|_| Stdio::inherit());
+
+        let stderr: Stdio = OpenOptions::new()
+            .write(true)
+            .open("/dev/tty")
+            .map(Stdio::from)
+            .unwrap_or_else(|_| Stdio::inherit());
+
+        let status = Command::new("cargo")
+            .current_dir(&path)
+            .env_clear()
+            .envs(&filtered_env)
+            .stdout(stdout)
+            .stderr(stderr)
+            .arg("+nightly")
+            .arg("build")
             .args(profile)
-            .arg("--path")
-            .arg(&path)
-            .arg("--root")
-            .arg(&out_dir)
+            .arg("--target-dir")
+            .arg(&target_dir)
+            .arg("--target")
+            .arg(target_name)
+            .arg("--bin")
+            .arg(&shim_name)
             .status()
             .expect("failed to build shim");
+
+        if !status.success() {
+            eprintln!("Failed to build shim {}", path);
+            std::process::exit(1);
+        }
+
+        let out_bin = out_dir_bin.join(&shim_name);
+
+        let shim_out_bin = shim_out_dir
+            .join(&target_name)
+            .join(&std::env::var("PROFILE").unwrap())
+            .join(&shim_name);
+
+        std::fs::rename(&shim_out_bin, &out_bin).expect("move failed");
     }
 }
